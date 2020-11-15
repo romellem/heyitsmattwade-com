10a06fffa702762a6452c7b87f93f6279e5e8b50
romellem <romellem.git@gmail.com>
2020-11-15 16:44:52 -0600

    Fixes static files build

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,12 +1,11 @@
 const glob = require('globby');
 
 module.exports = function (eleventyConfig) {
-	const non_compiled_files = glob.sync([
-		'./src/*',
-		'!./src/scripts/',
-		'!./src/styles/',
-		'!./src/index.html',
-	]);
+	const non_compiled_files = glob.sync(
+		['./src/**', '!./src/scripts/', '!./src/styles/', '!./src/index.html', '!./src/_includes'],
+		{ onlyFiles: false, deep: 1 }
+	);
+
 	for (let to_copy of non_compiled_files) {
 		eleventyConfig.addPassthroughCopy(to_copy);
 	}
