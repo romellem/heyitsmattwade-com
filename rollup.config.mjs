fed6a9f42f640d2e2221fb12fb062f39e8762634
romellem <romellem.git@gmail.com>
2026-05-02 11:23:54 -0500

    Upgrade rollup to rollup 3

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 
 const config = {
 	input: 'src/scripts/main.js',
@@ -24,23 +24,6 @@ const config = {
 		terser(),
 	],
 };
-
-// const serve_config = {
-// 	...config,
-// };
-// serve_config.plugins = [
-// 	...serve_config.plugins,
-// 	serve({
-// 		open: true,
-// 		contentBase: ['dist'],
-// 		host: 'localhost',
-// 		port: '3030',
-// 	}),
-// 	livereload({
-// 		watch: 'dist',
-// 		verbose: false,
-// 	}),
-// ];
 
 const configurations = [];
 
